d8c02324960640890c603450871700d95f82f089
İ. Emre Kutlu <emrekutlu@gmail.com>
2014-03-14 00:28:00 +0200

    don't always add space to the begining of via entity

--- lib/bluebird/strategies/via/strategy.rb
+++ lib/bluebird/strategies/via/strategy.rb
@@ -6,6 +6,7 @@ module Bluebird
 
           def run(tweet, config)
             if run?(config.via.username)
+              add_separator(tweet)
               tweet.add_partial(text(config.via), :via)
             end
           end
@@ -16,8 +17,14 @@ module Bluebird
             username_exists?(username) ? true : raise('Bluebird: config.via.username is missing.')
           end
 
+          def add_separator(tweet)
+            if add_separator?(tweet)
+              add_separator_partial?(tweet) ? add_separator_partial(tweet) : add_space_to_last_partial(tweet)
+            end
+          end
+
           def text(via_config)
-            " #{via_config.prefix} @#{via_config.username}"
+            "#{via_config.prefix} @#{via_config.username}"
           end
 
           def username_exists?(username)
@@ -26,6 +33,25 @@ module Bluebird
               !username.eql?('')
             else
               false
+            end
+          end
+
+          def add_separator_partial?(tweet)
+            !tweet.partials.last.text?
+          end
+
+          def add_separator?(tweet)
+            tweet.partials.length > 0
+          end
+
+          def add_separator_partial(tweet)
+            tweet.add_partial(' ', :text)
+          end
+
+          def add_space_to_last_partial(tweet)
+            last = tweet.partials.last
+            unless last.content.end_with?(' ')
+              last.content += ' '
             end
           end
 
--- spec/strategies/via/strategy_spec.rb
+++ spec/strategies/via/strategy_spec.rb
@@ -28,21 +28,58 @@ describe Bluebird::Strategies::Via::Strategy do
   end
 
   describe '#text' do
-    it 'adds a space to the begining' do
-      config = Bluebird::Strategies::Via::Config
-      config.username = 'iekutlu'
-      expect(strategy.send(:text, config).start_with?(' ')).to be_true
-    end
     it "adds 'prefix' to the begining" do
       config = Bluebird::Strategies::Via::Config
       config.username = 'iekutlu'
       config.prefix   = 'via'
-      expect(strategy.send(:text, config).start_with?(' via')).to be_true
+      expect(strategy.send(:text, config).start_with?('via')).to be_true
     end
     it 'adds username to the end' do
       config = Bluebird::Strategies::Via::Config
       config.username = 'iekutlu'
-      expect(strategy.send(:text, config).end_with?('iekutlu')).to be_true
+      expect(strategy.send(:text, config).end_with?('@iekutlu')).to be_true
+    end
+  end
+
+  describe '#add_separator?' do
+    context 'When tweet is not empty' do
+      it 'returns true' do
+        tweet = Bluebird::Tweet.new('Lorem ipsum')
+        expect(strategy.send(:add_separator?, tweet)).to be_true
+      end
+    end
+    context 'When tweet is empty' do
+      it 'returns false' do
+        tweet = Bluebird::Tweet.new('')
+        expect(strategy.send(:add_separator?, tweet)).to be_false
+      end
+    end
+  end
+
+  describe '#add_separator_partial' do
+    it "adds a new text partial whose content is ' '" do
+      tweet = Bluebird::Tweet.new('Lorem #ipsum')
+      strategy.send(:add_separator_partial, tweet)
+      last = tweet.partials.last
+      expect(last.text?).to be_true
+      expect(last.content).to eq ' '
+    end
+  end
+
+  describe '#add_space_to_last_partial' do
+    context 'When text partial has trailing space' do
+      it 'does not add trailing space' do
+        tweet = Bluebird::Tweet.new('Lorem ipsum ')
+        strategy.send(:add_space_to_last_partial, tweet)
+        expect(tweet.status).to eq 'Lorem ipsum '
+      end
+    end
+    context 'When text partial does not have trailing space' do
+      it 'adds trailing space' do
+        tweet = Bluebird::Tweet.new('Lorem ipsum')
+        strategy.send(:add_space_to_last_partial, tweet)
+        expect(tweet.status).to eq 'Lorem ipsum '
+      end
     end
   end
 end
